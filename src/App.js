a5e2f02b22a5bebdc34a7bb92059062c52a22355
M-Afan <afansaeed19971719@gmail.com>
2023-12-04 06:08:53 +0500

    Basic-Ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionLIst } from './components/TransactionLIst';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionLIst />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
